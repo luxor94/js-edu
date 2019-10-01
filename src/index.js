26b47d580e1cffddb725dd3b121033fba6e23658
Aliaksandr Papov <47752490+luxor94@users.noreply.github.com>
2019-10-01 22:10:52 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+    const learnHours = knowsProgramming ? 800 : 1300;
+    return Math.ceil(learnHours / config[focus]);
+};
+
+
